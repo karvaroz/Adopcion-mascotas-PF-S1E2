2b71e6083641c033af5b7a58b21ce6a247a10093
karvaroz <vargasriosk@gmail.com>
2022-04-13 12:33:56 -0500

    Arreglando script de datos

---
+++ scripts/login.js
@@ -0,0 +1,25 @@
+let form = document.getElementById("loginForm");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let emailU = document.getElementById("email").value;
+  let passwordU = document.getElementById("password").value;
+
+  fetch("http://localhost:4000/Usuarios", {
+    method: "POST",
+    body: JSON.stringify({
+      userEmail: emailU,
+      userPassword: passwordU,
+    }),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  })
+    .then(() => {
+      window.location.href = "../pages/find.html";
+      alert("La información se guardó correctamente");
+    })
+    .catch((err) => {
+      alert(err);
+    });
+});
