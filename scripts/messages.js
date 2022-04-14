8b1275fe9d873357cbd7384f9fa48069408d1f27
karvaroz <vargasriosk@gmail.com>
2022-04-14 16:54:53 -0500

    Guardado de mensaje

---
+++ scripts/messages.js
@@ -0,0 +1,28 @@
+let formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let mensaje = document.getElementById("mensaje").value;
+
+  fetch("https://api-pets-app.herokuapp.com/mensajes", {
+    method: "POST",
+    body: JSON.stringify({
+      mensaje: mensaje,
+    }),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  })
+    .then(() => {
+      Swal.fire("Guardado!", "Hemos recibido tu mensaje!", "success");
+    })
+    .catch((err) => {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Ha ocurrido un error!",
+        showConfirmButton: true,
+        footer: err,
+      });
+    });
+});
