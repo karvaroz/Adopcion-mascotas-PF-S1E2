96c156cc6b61a67dd566efb832edaf84788f4632
karvaroz <vargasriosk@gmail.com>
2022-04-14 16:59:34 -0500

    Subiendo cambios

--- scripts/script.js
+++ scripts/script.js
@@ -19,22 +19,24 @@ const getData = async (url) => {
     const { id, name, image, breed } = element;
     listarCard.innerHTML += `
         <div class="card" id=${id}>
-          <div class="card_img" style="
-              background: linear-gradient(
-                  180deg,
-                  rgba(255, 255, 255, 0) 26.42%,
-                  #000000 99.33%
-                ),
-                url(${image});
-              background-repeat: no-repeat;
-              background-position: center;
-              background-size: cover;
-            ">
-            <div class="card_info">
-              <h3>${name}</h3>
-              <p>${breed}</p>
+          <a href="/pages/detail.html">
+            <div class="card_img" style="
+                background: linear-gradient(
+                    180deg,
+                    rgba(255, 255, 255, 0) 26.42%,
+                    #000000 99.33%
+                  ),
+                  url(${image});
+                background-repeat: no-repeat;
+                background-position: center;
+                background-size: cover;
+              ">
+              <div class="card_info">
+                <h3>${name}</h3>
+                <p>${breed}</p>
+              </div>
             </div>
-          </div>
+          </a>
         </div>
     `;
   });
